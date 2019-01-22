a1480b2e712df72e34949e3b0dd0e0e7644efc24
daizongzhang <daizong.zhang@cybex.io>
2019-01-22 18:22:01 +0800

    更新订单通知接口

--- controllers/jpnotiController.go
+++ controllers/jpnotiController.go
@@ -5,12 +5,23 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"math/big"
 	"net/http"
 	"os"
 	"reflect"
+	"runtime/debug"
+	"strconv"
 
+	"github.com/jinzhu/gorm"
+
+	"github.com/cockroachdb/apd"
+
+	model "git.coding.net/bobxuyang/cy-gateway-BN/models"
+	exevent "git.coding.net/bobxuyang/cy-gateway-BN/repository/exevent"
+	exorder "git.coding.net/bobxuyang/cy-gateway-BN/repository/exorder"
+	order "git.coding.net/bobxuyang/cy-gateway-BN/repository/order"
 	utils "git.coding.net/bobxuyang/cy-gateway-BN/utils"
 	"github.com/btcsuite/btcd/btcec"
 	"golang.org/x/crypto/sha3"
@@ -86,41 +97,111 @@ func OrderNoti(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
 		return
 	}
-	// btc类的，如果订单发送给多个地址，n放在data结构里，需要解析取出来
-	n := 0
-	if result.CoinType == "BTC" {
-		toes := result.Data["to"]
-		switch toes.(type) {
-		case []interface{}:
-			toesMap := toes.([]interface{})
-			if len(toesMap) <= 1 {
-				break
-			}
-			for _, v := range toesMap {
-				val := v.(map[string]interface{})
-				if val["address"].(string) != result.To {
-					continue
-				}
-				switch val["n"].(type) {
-				case float64:
-					n = int(val["n"].(float64))
-				case int64:
-					n = int(val["n"].(int64))
-				case int32:
-					n = int(val["n"].(int32))
-				}
-				break
-			}
-		default:
-			utils.Errorf("data.to type  is: %v", reflect.TypeOf(toes))
+
+	tx := model.GetDB().Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			utils.Errorf("%v, stack: %s", r, debug.Stack())
+			tx.Rollback()
+		}
+	}()
+
+	// save exevent
+	exeventEntity := new(model.ExEvent)
+	exeventEntity.AssetID = 1
+	exeventEntity.Hash = result.Hash
+	exeventEntity.JadepoolID = 1
+	exeventEntity.Status = result.State
+	exeventEntity.Log = string(requestBody)
+	exeventRepo := exevent.NewRepo(tx)
+	err = exeventRepo.Create(exeventEntity)
+	if err != nil {
+		tx.Rollback()
+		utils.Errorf("error: %v", err)
+		w.WriteHeader(400)
+		return
+	}
+
+	// find exorder and save/update exorder
+	jpOrderID, err := strconv.Atoi(result.ID)
+	if err != nil {
+		tx.Rollback()
+		utils.Errorf("atoi error: %v, id: %s", err, result.ID)
+		w.WriteHeader(400)
+		return
+	}
+	exorderRepo := exorder.NewRepo(tx)
+	exorderEntity, err := exorderRepo.GetByJPID(uint(jpOrderID))
+	if err != nil && !gorm.IsRecordNotFoundError(err) {
+		tx.Rollback()
+		utils.Errorf("get exorder error: %v", err)
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("GetByJPID", exorderEntity, err)
+
+	if exorderEntity == nil {
+		// btc类的，如果订单发送给多个地址，n放在data结构里，需要解析取出来
+		n := parseIndexFromResult(result)
+
+		exorderEntity = new(model.ExOrder)
+		exorderEntity.From = result.From
+		exorderEntity.To = result.To
+		exorderEntity.Hash = result.Hash
+		exorderEntity.UUHash = fmt.Sprintf("%s:%s:%d", result.CoinType, result.Hash, n)
+		exorderEntity.Index = n
+		exorderEntity.JadepoolOrderID = uint(jpOrderID)
+		exorderEntity.Status = result.State
+		exorderEntity.Type = result.BizType
+		//todo: 查询实际的id
+		exorderEntity.AssetID = 1
+		//todo: 查询实际的id
+		exorderEntity.JadepoolID = 1
+		amount, condition, err := apd.NewFromString(result.Value)
+		if err != nil || condition.Any() {
+			tx.Rollback()
+			utils.Errorf("apd.NewFromString error: %v, condition: %s", err, condition.String())
+			w.WriteHeader(400)
+			return
+		}
+		exorderEntity.Amount = amount
+		err = exorderRepo.Create(exorderEntity)
+		if err != nil {
+			tx.Rollback()
+			utils.Errorf("create exorder error: %v", err)
+			w.WriteHeader(400)
+			return
+		}
+	} else {
+		// todo: save state
+		exorderEntity.Status = result.State
+	}
+
+	// 链上已经确认，可以创建order
+	if exorderEntity.Status == "done" {
+		orderRepo := order.NewRepo(tx)
+		orderEntity := new(model.Order)
+		orderEntity.From = exorderEntity.From
+		orderEntity.Hash = exorderEntity.Hash
+		orderEntity.Index = exorderEntity.Index
+		orderEntity.Status = "done"
+		orderEntity.To = exorderEntity.To
+		orderEntity.Type = exorderEntity.Type
+		orderEntity.UUHash = exorderEntity.UUHash
+		orderEntity.AssetID = exorderEntity.AssetID
+		orderEntity.Amount = exorderEntity.Amount
+		//todo: 查询实际的id
+		orderEntity.AppID = 1
+		err = orderRepo.Create(orderEntity)
+		if err != nil {
+			tx.Rollback()
+			utils.Errorf("create order error: %v", err)
+			w.WriteHeader(400)
+			return
 		}
 	}
-	n = n + 0
-	//todo: save log
+	tx.Commit()
 
-	//todo: find exorder and save/update exorder
-
-	//todo: response
 	resp := utils.Message(true, "success")
 	utils.Respond(w, resp)
 }
@@ -166,4 +247,37 @@ func verifySign(result *OrderNotiResult, sign *Sig, pubkey string) (bool, error)
 		S: new(big.Int).SetBytes(decodedS),
 	}
 	return signature.Verify(msgBuf, pubKey), nil
+}
+
+func parseIndexFromResult(result *OrderNotiResult) int {
+	n := 0
+	if result.CoinType == "BTC" {
+		toes := result.Data["to"]
+		switch toes.(type) {
+		case []interface{}:
+			toesMap := toes.([]interface{})
+			if len(toesMap) <= 1 {
+				break
+			}
+			for _, v := range toesMap {
+				val := v.(map[string]interface{})
+				if val["address"].(string) != result.To {
+					continue
+				}
+				switch val["n"].(type) {
+				case float64:
+					n = int(val["n"].(float64))
+				case int64:
+					n = int(val["n"].(int64))
+				case int32:
+					n = int(val["n"].(int32))
+				}
+				break
+			}
+		default:
+			utils.Errorf("data.to type  is: %v", reflect.TypeOf(toes))
+		}
+	}
+
+	return n
 }
