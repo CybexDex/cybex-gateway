76fd928c01faee3fa28b63ec3f9c6eaedf6666b1
xuyang <bobxuyang@qq.com>
2019-01-23 23:50:25 +0800

    half change

--- models/account.go
+++ models/account.go
@@ -31,3 +31,66 @@ type Account struct {
 	Type            string `gorm:"type:varchar(32);default:'SAAS_USER'" json:"type"` // ADMIN, SUPER_ADMIN, SAAS_USER
 	Disable         bool   `gorm:"default:false" json:"disable"`
 }
+
+//FetchAll ...
+func (acc *Account) FetchAll() ([]*Account, error) {
+	var res []*Account
+	err := GetDB().Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//Fetch ...
+func (acc *Account) Fetch(p Page) (res []*Account, err error) {
+	err = GetDB().Order(p.OrderBy + " " + p.Sort).Offset(p.Offset).Find(&res).Limit(p.Amount).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//GetByID ...
+func (acc *Account) GetByID(id uint) (*Account, error) {
+	a := Account{}
+	err := GetDB().First(&a, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//GetByName ...
+func (acc *Account) GetByName(name string) (*Account, error) {
+	a := Account{}
+	err := GetDB().Where("name=?", name).First(&a).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//Update ...
+func (acc *Account) Update(id uint, v *Account) error {
+	return GetDB().Model(Account{}).Where("ID=?", id).UpdateColumns(v).Error
+}
+
+//Create ...
+func (acc *Account) Create(a *Account) (err error) {
+	return GetDB().Create(&a).Error
+}
+
+//DeleteByID ...
+func (acc *Account) DeleteByID(id uint) (err error) {
+	return GetDB().Where("ID=?", id).Delete(&Account{}).Error
+}
+
+//Delete ...
+func (acc *Account) Delete(a *Account) (err error) {
+	return GetDB().Delete(&a).Error
+}
--- models/address.go
+++ models/address.go
@@ -17,3 +17,54 @@ type Address struct {
 	// memo      string `gorm:"type:varchar(64)" json:"memo"`
 	// UUAddress string `gorm:"type:varchar(255);not null" json:"uuaddres"` // = BLOCKCHAIN + ADDRESS
 }
+
+//FetchAll ...
+func (add *Address) FetchAll() (res []*Address, err error) {
+	err = GetDB().Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//Fetch ...
+func (add *Address) Fetch(p Page) (res []*Address, err error) {
+	err = GetDB().Order(p.OrderBy + " " + p.Sort).Offset(p.Offset).Find(&res).Limit(p.Amount).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//GetByID ...
+func (add *Address) GetByID(id uint) (*Address, error) {
+	a := Address{}
+	err := GetDB().First(&a, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//Update ...
+func (add *Address) Update(id uint, v *Address) error {
+	return GetDB().Model(Address{}).Where("ID=?", id).UpdateColumns(v).Error
+}
+
+//Create ...
+func (add *Address) Create(a *Address) (err error) {
+	return GetDB().Create(&a).Error
+}
+
+//DeleteByID ...
+func (add *Address) DeleteByID(id uint) (err error) {
+	return GetDB().Where("ID=?", id).Delete(&Address{}).Error
+}
+
+//Delete ...
+func (add *Address) Delete(a *Address) (err error) {
+	return GetDB().Delete(&a).Error
+}
