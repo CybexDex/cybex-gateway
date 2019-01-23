0c25f00748373a8cf1671a350cc652cb3e34f8bb
xuyang <yang.xu@nbltrust.com>
2019-01-23 11:02:03 +0800

    revert base.go

--- models/base.go
+++ models/base.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jinzhu/gorm"
 
@@ -19,10 +20,15 @@ func init() {
 		fmt.Print(e)
 	}
 
-	username := "postgres"      // os.Getenv("db_user")
-	password := "your_password" // os.Getenv("db_pass")
-	dbName := "demo"            // os.Getenv("db_name")
-	dbHost := "47.100.239.204"  // os.Getenv("db_host")
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	// username := "postgres"      // os.Getenv("db_user")
+	// password := "your_password" // os.Getenv("db_pass")
+	// dbName := "demo"            // os.Getenv("db_name")
+	// dbHost := "47.100.239.204"  // os.Getenv("db_host")
 
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
 	fmt.Println(dbURI)
