4359700b79ecb11f6b5da972fc2762258231ef3f
xuyang <yang.xu@nbltrust.com>
2019-01-25 11:55:45 +0800

    update order model

--- models/order.go
+++ models/order.go
@@ -14,6 +14,10 @@ const (
 	OrderStatusDone = "DONE"
 	// OrderStatusTerminated ...
 	OrderStatusTerminated = "TERMINATED"
+	// OrderTypeDeposit ...
+	OrderTypeDeposit = "DEPOSIT"
+	// OrderTypeWithdraw ...
+	OrderTypeWithdraw = "WITHDRAW"
 )
 
 //Order ...
@@ -30,12 +34,18 @@ type Order struct {
 	// BlockchainFee *apd.Decimal `gorm:"type:numeric(30,10)" json:"blockchainFee"` //
 	// Settled     bool         `gorm:"default:false" json:"settled"`
 
+	JPHash   string `gorm:"index;type:varchar(128)" json:"jpHash"`
+	JPUUHash string `gorm:"type:varchar(256)" json:"jpUUHash"` // = BLOCKCHAINNAME + HASH + INDEX (if INDEX is null then ignore)
+
+	CYBHash   string `gorm:"index;type:varchar(128)" json:"cybHash"`
+	CYBUUHash string `gorm:"type:varchar(256)" json:"cybUUHash"` // = BLOCKCHAINNAME + HASH + INDEX (if INDEX is null then ignore)
+
 	TotalAmount *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"totalAmount"` // totalAmount = amount + fee
 	Amount      *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"amount"`
 	Fee         *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"fee"` // fee in Asset
 
 	Status string `gorm:"type:varchar(32);not null" json:"status"` // INIT, PROCESSING, DONE, TERMINATED
-	Type   string `gorm:"type:varchar(32);not null" json:"type"`   // DEPOSIT, WITHDRAW, RECHARGE, INTERNAL
+	Type   string `gorm:"type:varchar(32);not null" json:"type"`   // DEPOSIT, WITHDRAW
 }
 
 //UpdateColumns ...
