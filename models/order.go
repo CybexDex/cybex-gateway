0d9b6621d1476d8d0f55295be06b38b21061c9db
xuyang <yang.xu@nbltrust.com>
2019-01-25 11:32:21 +0800

    update order removing blockchain related stuff

--- models/order.go
+++ models/order.go
@@ -10,20 +10,8 @@ const (
 	OrderStatusInit = "INIT"
 	// OrderStatusProcessing ...
 	OrderStatusProcessing = "PROCESSING"
-	// OrderStatusProcessed ...
-	OrderStatusProcessed = "PROCESSED"
-	// OrderStatusHolding ...
-	OrderStatusHolding = "HOLDING"
-	// OrderStatusPending ...
-	OrderStatusPending = "PENDING"
 	// OrderStatusDone ...
 	OrderStatusDone = "DONE"
-	// OrderStatusFailed ...
-	OrderStatusFailed = "FAILED"
-	// OrderStatusContinue ...
-	OrderStatusContinue = "CONTINUE"
-	// OrderStatusRetry ...
-	OrderStatusRetry = "RETRY"
 	// OrderStatusTerminated ...
 	OrderStatusTerminated = "TERMINATED"
 )
@@ -35,17 +23,19 @@ type Order struct {
 	AssetID uint `gorm:"not null" json:"assetID"` // 1 to n
 	AppID   uint `gorm:"not null" json:"appID"`   // 1 to n
 
-	Accounting      Accounting `gorm:"foreignkey:AccountingRefer" json:"accounting"` // 1 to 1
-	AccountingRefer uint       `json:"accountingRefer"`
+	// Accounting      Accounting `gorm:"foreignkey:AccountingRefer" json:"accounting"` // 1 to 1
+	// AccountingRefer uint       `json:"accountingRefer"`
 
-	From   string       `gorm:"type:varchar(128)" json:"from"`
-	To     string       `gorm:"type:varchar(128)" json:"to"`
-	Amount *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"amount"`
-	Index  int          `json:"index"`
-	Hash   string       `gorm:"index;type:varchar(128);not null" json:"hash"`
-	UUHash string       `gorm:"type:varchar(256);not null" json:"uuhash"` // = BLOCKCHAINNAME + HASH + INDEX (if INDEX is null then ignore)
-	Status string       `gorm:"type:varchar(32);not null" json:"status"`  // INIT, PROCESSING, PROCESSED, HOLDING, PENDING, DONE, FAILED, CONTINUE, RETRY, TERMINATED
-	Type   string       `gorm:"type:varchar(32);not null" json:"type"`    // DEPOSIT, WITHDRAW, RECHARGE, INTERNAL
+	// Amount        *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"amount"`
+	// BlockchainFee *apd.Decimal `gorm:"type:numeric(30,10)" json:"blockchainFee"` //
+	// Settled     bool         `gorm:"default:false" json:"settled"`
+
+	TotalAmount *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"totalAmount"` // totalAmount = amount + fee
+	Amount      *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"amount"`
+	Fee         *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"fee"` // fee in Asset
+
+	Status string `gorm:"type:varchar(32);not null" json:"status"` // INIT, PROCESSING, DONE, TERMINATED
+	Type   string `gorm:"type:varchar(32);not null" json:"type"`   // DEPOSIT, WITHDRAW, RECHARGE, INTERNAL
 }
 
 //UpdateColumns ...
