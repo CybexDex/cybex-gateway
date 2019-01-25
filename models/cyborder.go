d2cbfbfaa290d1cf2de0c5add706aa21f7837c0e
xuyang <yang.xu@nbltrust.com>
2019-01-25 12:00:43 +0800

    add cyborder model and repo

---
+++ models/cyborder.go
@@ -0,0 +1,61 @@
+package model
+
+import (
+	"github.com/cockroachdb/apd"
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	// CybOrderStatusInit ...
+	CybOrderStatusInit = "INIT"
+	// CybOrderStatusHolding ...
+	CybOrderStatusHolding = "HOLDING"
+	// CybOrderStatusPending ...
+	CybOrderStatusPending = "PENDING"
+	// CybOrderStatusDone ...
+	CybOrderStatusDone = "DONE"
+	// CybOrderStatusFailed ...
+	CybOrderStatusFailed = "FAILED"
+)
+
+//CybOrder ...
+type CybOrder struct {
+	gorm.Model
+
+	AssetID uint `gorm:"not null" json:"assetID"` // 1 to n
+	AppID   uint `gorm:"not null" json:"appID"`   // 1 to n
+
+	Accounting      Accounting `gorm:"foreignkey:AccountingRefer" json:"accounting"` // 1 to 1
+	AccountingRefer uint       `json:"accountingRefer"`
+
+	From   string       `gorm:"type:varchar(128)" json:"from"`
+	To     string       `gorm:"type:varchar(128)" json:"to"`
+	Amount *apd.Decimal `gorm:"type:numeric(30,10);not null" json:"amount"`
+	CybFee *apd.Decimal `gorm:"type:numeric(30,10)" json:"cybFee"` // cybex blockchain fee
+	// FeeAssetID uint
+
+	Hash   string `gorm:"index;type:varchar(128);not null" json:"hash"`
+	UUHash string `gorm:"type:varchar(256);not null" json:"uuhash"` // = BLOCKCHAINNAME + HASH + INDEX (if INDEX is null then ignore)
+	Status string `gorm:"type:varchar(32);not null" json:"status"`  // INIT, HOLDING, PENDING, DONE, FAILED
+	Type   string `gorm:"type:varchar(32);not null" json:"type"`    // DEPOSIT, WITHDRAW, RECHARGE, SWEEP, FEESETTLE
+}
+
+//UpdateColumns ...
+func (a *CybOrder) UpdateColumns(b *CybOrder) error {
+	return GetDB().Model(CybOrder{}).Where("ID=?", a.ID).UpdateColumns(b).Error
+}
+
+//Create ...
+func (a *CybOrder) Create() (err error) {
+	return GetDB().Create(&a).Error
+}
+
+//Save ...
+func (a *CybOrder) Save() (err error) {
+	return GetDB().Save(&a).Error
+}
+
+//Delete ...
+func (a *CybOrder) Delete() (err error) {
+	return GetDB().Delete(&a).Error
+}
---
+++ repository/cyborder/cyborder.go
@@ -0,0 +1,87 @@
+package cyborder
+
+import (
+	m "git.coding.net/bobxuyang/cy-gateway-BN/models"
+	r "git.coding.net/bobxuyang/cy-gateway-BN/repository"
+	"github.com/jinzhu/gorm"
+)
+
+//Repository ...
+type Repository interface {
+	FetchAll() ([]*m.CybOrder, error)
+	Fetch(p r.Page) ([]*m.CybOrder, error)
+	FetchWith(o *m.CybOrder) ([]*m.CybOrder, error)
+	GetByID(id uint) (*m.CybOrder, error)
+	DeleteByID(id uint) error
+	Create(a *m.CybOrder) (err error)
+}
+
+//Repo ...
+type Repo struct {
+	DB *gorm.DB
+}
+
+//NewRepo ...
+func NewRepo(db *gorm.DB) Repository {
+	return &Repo{
+		DB: db,
+	}
+}
+
+//FetchAll ...
+func (repo *Repo) FetchAll() ([]*m.CybOrder, error) {
+	var res []*m.CybOrder
+	err := repo.DB.Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//Fetch ...
+func (repo *Repo) Fetch(p r.Page) (res []*m.CybOrder, err error) {
+	err = repo.DB.CybOrder(p.OrderBy + " " + p.Sort).Offset(p.Offset).Find(&res).Limit(p.Amount).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//FetchWith ...
+func (repo *Repo) FetchWith(o *m.CybOrder) (res []*m.CybOrder, err error) {
+	err = repo.DB.Where(o).Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//GetByID ...
+func (repo *Repo) GetByID(id uint) (*m.CybOrder, error) {
+	a := m.CybOrder{}
+	err := repo.DB.First(&a, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//DeleteByID ...
+func (repo *Repo) DeleteByID(id uint) error {
+	return repo.DB.Where("ID=?", id).Delete(&m.CybOrder{}).Error
+}
+
+//Create ...
+//for transaction base use
+func (repo *Repo) Create(a *m.CybOrder) (err error) {
+	err = repo.DB.Create(&a).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
