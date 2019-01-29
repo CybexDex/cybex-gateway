507a34813025af87bbbfd784be9b3bfd9fd37f48
xuyang <yang.xu@nbltrust.com>
2019-01-29 10:51:06 +0800

    add more usage code for helper_test

--- models/helper_test.go
+++ models/helper_test.go
@@ -1,8 +1,9 @@
 package model
 
 import (
-	"git.coding.net/bobxuyang/cy-gateway-BN/utils"
 	"testing"
+
+	"git.coding.net/bobxuyang/cy-gateway-BN/utils"
 
 	"github.com/cockroachdb/apd"
 )
@@ -15,9 +16,26 @@ func TestUpdateBalance(t *testing.T) {
 	//fmt.Println(bal)
 
 	data := GetBalanceInitData()
+
 	data["Balance"].Oper = "ADD"
-	one := apd.New(123, 0)
+	one := apd.New(1, 0)
 	data["Balance"].Value = one
+
+	data["InLocked"].Oper = "ADD"
+	two := apd.New(2, 0)
+	data["InLocked"].Value = two
+
+	data["OutLocked"].Oper = "SUB"
+	three := apd.New(3, 0)
+	data["OutLocked"].Value = three
+
+	data["InLockedFee"].Oper = "ADD"
+	four := apd.New(123, 0)
+	data["InLockedFee"].Value = four
+
+	data["OutLockedFee"].Oper = "SUB"
+	five := apd.New(123, 0)
+	data["OutLocked"].Value = five
 
 	err := ComputeBalance(db, &bal, &data)
 	if err != nil {
