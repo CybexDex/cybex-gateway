7396110af2b31668c64ac36fcc9472138a919c7d
xuyang <yang.xu@nbltrust.com>
2019-02-05 21:17:43 +0800

    add balance repo

--- repository/balance/balance.go
+++ repository/balance/balance.go
@@ -1,1 +1,87 @@
 package balance
+
+import (
+	m "git.coding.net/bobxuyang/cy-gateway-BN/models"
+	r "git.coding.net/bobxuyang/cy-gateway-BN/repository"
+	"github.com/jinzhu/gorm"
+)
+
+//Repository ...
+type Repository interface {
+	FetchAll() ([]*m.Balance, error)
+	Fetch(p r.Page) ([]*m.Balance, error)
+	FetchWith(o *m.Balance) ([]*m.Balance, error)
+	GetByName(name string) (*m.Balance, error)
+	GetByID(id uint) (*m.Balance, error)
+	DeleteByID(id uint) error
+}
+
+//Repo ...
+type Repo struct {
+	DB *gorm.DB
+}
+
+//NewRepo ...
+func NewRepo(db *gorm.DB) Repository {
+	return &Repo{
+		DB: db,
+	}
+}
+
+//FetchAll ...
+func (repo *Repo) FetchAll() ([]*m.Balance, error) {
+	var res []*m.Balance
+	err := repo.DB.Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//Fetch ...
+func (repo *Repo) Fetch(p r.Page) (res []*m.Balance, err error) {
+	err = repo.DB.Order(p.OrderBy + " " + p.Sort).Offset(p.Offset).Find(&res).Limit(p.Amount).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//FetchWith ...
+func (repo *Repo) FetchWith(o *m.Balance) (res []*m.Balance, err error) {
+	err = repo.DB.Where(o).Find(&res).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return res, err
+}
+
+//GetByID ...
+func (repo *Repo) GetByID(id uint) (*m.Balance, error) {
+	a := m.Balance{}
+	err := repo.DB.First(&a, id).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//GetByName ...
+func (repo *Repo) GetByName(name string) (*m.Balance, error) {
+	a := m.Balance{}
+	err := repo.DB.Where("name=?", name).First(&a).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &a, err
+}
+
+//DeleteByID ...
+func (repo *Repo) DeleteByID(id uint) error {
+	return repo.DB.Where("ID=?", id).Delete(&m.Balance{}).Error
+}
