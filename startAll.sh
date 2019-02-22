f3a9e0b4aa5cb3ffaad8a41b816899a9b55bd9b5
daizongzhang <daizong.zhang@cybex.io>
2019-02-22 15:57:58 +0800

    update startAll.sh

--- startAll.sh
+++ startAll.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+
+# exit if error
 set -e
 
+# create log dir
 if [ ! -d "./log" ]; then 
     mkdir log
 fi
 
+# build every servers
 make buildJPSrv buildAdminSrv buildCybSrv buildOrderSrv buildUserSrv
 
+# kill & start jpsrv
 echo "start jpsrv..."
 ps aux | grep -ie "bin/jpsrv" | grep -v 'grep'| awk '{print $2}' | xargs kill -9
-nohup ./bin/jpsrv >./log/jpsrv.log 2>&1 &
+nohup ./bin/jpsrv >>./log/jpsrv.log 2>&1 &
 
+# kill & start adminsrv
 echo "start adminsrv..."
 ps aux | grep -ie "bin/adminsrv" | grep -v 'grep'| awk '{print $2}' | xargs kill -9
-nohup ./bin/adminsrv >./log/adminsrv.log 2>&1 &
+nohup ./bin/adminsrv >>./log/adminsrv.log 2>&1 &
 
+# kill & start cybsrv
 echo "start cybsrv..."
 ps aux | grep -ie "bin/cybsrv" | grep -v 'grep'| awk '{print $2}' | xargs kill -9
-nohup ./bin/cybsrv >./log/cybsrv.log 2>&1 &
+nohup ./bin/cybsrv >>./log/cybsrv.log 2>&1 &
 
+# kill & start ordersrv
 echo "start ordersrv..."
 ps aux | grep -ie "bin/ordersrv" | grep -v 'grep'| awk '{print $2}' | xargs kill -9
-nohup ./bin/ordersrv >./log/ordersrv.log 2>&1 &
+nohup ./bin/ordersrv >>./log/ordersrv.log 2>&1 &
 
+# kill & start usersrv
 echo "start usersrv..."
 ps aux | grep -ie "bin/usersrv" | grep -v 'grep'| awk '{print $2}' | xargs kill -9
-nohup ./bin/usersrv >./log/usersrv.log 2>&1 &
+nohup ./bin/usersrv >>./log/usersrv.log 2>&1 &
