1fe9d2114e5815a355efbaa375f97a5b6e0937aa
yang yu <yundkyy@qq.com>
2019-02-27 14:58:53 +0800

    log level config

--- cmd/cybsrv/main.go
+++ cmd/cybsrv/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "git.coding.net/bobxuyang/cy-gateway-BN/controllers/cybsrv"
+import (
+	"git.coding.net/bobxuyang/cy-gateway-BN/controllers/cybsrv"
+	"git.coding.net/bobxuyang/cy-gateway-BN/utils"
+	"github.com/spf13/viper"
+)
+
+var (
+	githash   string
+	buildtime string
+	branch    string
+)
 
 func main() {
+	// init config
+	utils.InitConfig()
+	logDir := viper.GetString("cybsrv.log_dir")
+	logLevel := viper.GetString("cybsrv.log_level")
+	// init logger
+	utils.InitLog(logDir, logLevel)
+	utils.Infof("build info: %s_%s_%s", buildtime, branch, githash)
 	// cybsrv.Test()
 	go cybsrv.BlockRead()
 	go cybsrv.HandleWorker()
--- cmd/ordersrv/main.go
+++ cmd/ordersrv/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"git.coding.net/bobxuyang/cy-gateway-BN/controllers/ordersrv"
+	"git.coding.net/bobxuyang/cy-gateway-BN/utils"
+	"github.com/spf13/viper"
+)
+
+var (
+	githash   string
+	buildtime string
+	branch    string
 )
 
 func main() {
+	utils.InitConfig()
+	logDir := viper.GetString("ordersrv.log_dir")
+	logLevel := viper.GetString("ordersrv.log_level")
+	// init logger
+	utils.InitLog(logDir, logLevel)
+	utils.Infof("build info: %s_%s_%s", buildtime, branch, githash)
 	ordersrv.HandleWorker()
 }
