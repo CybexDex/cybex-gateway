b45357556efef44f3a0c15e93b2c28930a4b7d2a
yang yu <yundkyy@qq.com>
2019-03-02 16:33:20 +0800

    不要重复init

--- utils/config.go
+++ utils/config.go
@@ -1,21 +1,29 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/viper"
 )
 
+var inited = false
+
 // InitConfig ...
 func InitConfig() {
-	env := os.Getenv("env")
+	if inited {
+		return
+	}
+	env := os.Getenv("GATEWAY_ENV")
 	if len(env) == 0 {
 		env = "dev"
 	}
+	fmt.Println("env is ", env)
 	viper.SetConfigName(env)
 	viper.AddConfigPath("./config")
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
+	inited = true
 }
