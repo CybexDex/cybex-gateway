634f52c6496c547824fe4e80ef9438846e2e3af4
yang yu <yundkyy@qq.com>
2019-07-09 14:28:54 +0800

    test ETH 获取地址

--- cmd/tester/main.go
+++ cmd/tester/main.go
@@ -12,11 +12,11 @@ import (
 )
 
 func main1() {
-	config.LoadConfig("uat")
+	config.LoadConfig("dev")
 	s, _ := jp.DepositAddress("ETH")
 	fmt.Println(s)
 }
-func main() {
+func main2() {
 	s := ecc.PriToPub("bf12996feeaa2977b6ca0d33a0e8bd2ccfc4844c6f8a7e6d15c099f8da4a255d")
 	fmt.Println(s)
 }
@@ -32,4 +32,7 @@ func main4() {
 }
 func main5() {
 	ecc.TestECCSign()
+}
+func main() {
+	main1()
 }
