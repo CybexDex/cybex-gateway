a61a526535da60f5ff9f8d4d8aec783918a951f2
yang yu <yundkyy@qq.com>
2019-07-17 09:57:53 +0800

    update from and log

--- server/jp/jpserver.go
+++ server/jp/jpserver.go
@@ -6,69 +6,72 @@ import (
 	"cybex-gateway/types"
 	"cybex-gateway/utils"
 	"cybex-gateway/utils/log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
-func jpNotify(c *gin.Context){
-			// 记录日志
-			reqBody := &types.JPEvent{}
-			err := c.Bind(&reqBody)
-			if err != nil {
-				log.Errorln("Error", err)
-				errorRes(c, 400, gin.H{
-					"message": "Unmarshal error",
-				})
-				return
-			}
-			err = jp.CheckComing(reqBody)
-			if err != nil {
-				log.Errorln("Error", err)
-				errorRes(c, 400, gin.H{
-					"message": err,
-				})
-				return
-			}
-			result := types.JPOrderResult{}
-			err = utils.ResultToStruct(reqBody.Result, &result)
-			if err != nil {
-				log.Errorln("Error", err)
-				errorRes(c, 400, gin.H{
-					"message": "Unmarshal Error",
-				})
-				return
-			}
-			if result.BizType == "WITHDRAW" {
-				// 提现订单
-				err = jp.HandleWithdraw(result)
-				if err != nil {
-					log.Errorln("Error", err)
-					errorRes(c, 400, gin.H{
-						"message": "HandleWithdraw Error",
-					})
-					return
-				}
-			} else if result.BizType == "DEPOSIT" {
-				// 充值订单
-				err = jp.HandleDeposit(result)
-				if err != nil {
-					log.Errorln("Error", err)
-					errorRes(c, 400, gin.H{
-						"message": "HandleDeposit Error",
-					})
-					return
-				}
-			} else {
-				// TODO 可能一直发无法处理的类型
-				errorRes(c, 400, gin.H{
-					"message": "BizType Error",
-				})
-				return
-			}
-			// 返回
-			c.JSON(200, gin.H{
-				"message": "pong",
+
+func jpNotify(c *gin.Context) {
+	// 记录日志
+	reqBody := &types.JPEvent{}
+	err := c.Bind(&reqBody)
+	if err != nil {
+		log.Errorln("Error", err)
+		errorRes(c, 400, gin.H{
+			"message": "Unmarshal error",
+		})
+		return
+	}
+	err = jp.CheckComing(reqBody)
+	if err != nil {
+		log.Errorln("Error CheckComing", err)
+		errorRes(c, 400, gin.H{
+			"message": err,
+		})
+		return
+	}
+	result := types.JPOrderResult{}
+	err = utils.ResultToStruct(reqBody.Result, &result)
+	if err != nil {
+		log.Errorln("Error", err)
+		errorRes(c, 400, gin.H{
+			"message": "Unmarshal Error",
+		})
+		return
+	}
+	if result.BizType == "WITHDRAW" {
+		// 提现订单
+		err = jp.HandleWithdraw(result)
+		if err != nil {
+			log.Errorln("Error HandleWithdraw", result.ID, err)
+			errorRes(c, 400, gin.H{
+				"message": "HandleWithdraw Error",
 			})
+			return
+		}
+	} else if result.BizType == "DEPOSIT" {
+		// 充值订单
+		err = jp.HandleDeposit(result)
+		if err != nil {
+			log.Errorln("Error HandleDeposit", result.ID, err)
+			errorRes(c, 400, gin.H{
+				"message": "HandleDeposit Error",
+			})
+			return
+		}
+	} else {
+		// TODO 可能一直发无法处理的类型
+		errorRes(c, 400, gin.H{
+			"message": "BizType Error",
+		})
+		return
+	}
+	// 返回
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
+
 // StartServer ...
 func StartServer() {
 	gin.SetMode(gin.ReleaseMode)
