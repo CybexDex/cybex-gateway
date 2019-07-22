61046cf2ba78708585d28c70966e9a6a92e12629
yang yu <yundkyy@qq.com>
2019-07-22 16:20:43 +0800

    update JadeName in admin

--- modeladmin/asset.go
+++ modeladmin/asset.go
@@ -14,6 +14,7 @@ import (
 type Asset struct {
 	gorm.Model
 	Name         string `gorm:"unique" json:"name"`
+	JadeName     string `json:"jadeName"`
 	Blockchain   string `json:"blockchain"`
 	Projectname  string `json:"projectname"`
 	CYBName      string `json:"cybname"`
